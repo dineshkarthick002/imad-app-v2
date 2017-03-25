68b0842a824664e0c8c4c258b12c4c14d0908a53
dineshkarthick002 <dineshkarthick0608@gmail.com>
2017-03-25 22:02:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,69 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+
+
+var articleone={
+    title:'article one |dinesh kumar',
+    heading:'article one',
+    date:'sep s,2016',
+    content:'
+    <p>
+                    this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+                </p>
+                 <p>
+                    this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+                </p>
+                 <p>
+                    this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.''
+                </p>
+};
+
+function createtemplate(data){
+    var title =data.title;
+    var data =data.date;
+    var heading =data.heading;
+    var content =data.content;
+}
+var htmltemplte =
+<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+        <a herf="/"home</a>
+            </div>
+            <hr/>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+          <div>
+          ${content}
+          </div>
+            
+    </body>
+</html>
+';
+return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function (req,res) {
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function (req,res) {
@@ -20,16 +77,5 @@ app.get('/article-three',function (req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
     
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+   
